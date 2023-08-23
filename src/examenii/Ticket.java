141519e98c4ca675ee7b9f2f7870b49a447cbdb3
EkarCortes <Ekardaco@gmail.com>
2023-08-23 09:08:09 -0600

    Clase ticket
    Se crea la clase ticket con sus metodos de acceso y su contructor

---
+++ src/examenii/Ticket.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenii;
+
+/**
+ *
+ * @author Student
+ */
+public class Ticket {
+    private int num;
+    private String tipo;
+
+    public Ticket(int num, String tipo) {
+        this.num = num;
+        this.tipo = tipo;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
